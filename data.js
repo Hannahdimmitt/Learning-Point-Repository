e4ce4b977c2237b1850da7750a1e49aeb0fbe399
monsharma02 <awake.ms5@gmail.com>
2022-11-03 16:42:56 -0500

    Developed student-recruiter discussion forum. Renamed peertopeer filenme.

---
+++ data.js
@@ -0,0 +1,48 @@
+var defaultThreads = [
+    {
+        id: 1,
+        title: "Skills Development",
+        author: "User",
+        date: Date.now(),
+        content: "Skills Dev content",
+        comments: [
+            {
+                author: "Monika",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Dr.Strange",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Get Hired!",
+        author: "User",
+        date: Date.now(),
+        content: "Get hired content",
+        comments: [
+            {
+                author: "Student",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Recruiter",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    }
+]
+
+var threads = defaultThreads
+if (localStorage && localStorage.getItem('Networking_DiscussionThreads')) {
+    threads = JSON.parse(localStorage.getItem('Networking_DiscussionThreads'));
+} else {
+    threads = defaultThreads;
+    localStorage.setItem('Networking_DiscussionThreads', JSON.stringify(defaultThreads));
+}
